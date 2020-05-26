9380110a78090dec4fb144bdad8597cbfe4883d6
rahul975337 <55583515+rahul975337@users.noreply.github.com>
2020-05-26 16:24:48 +0530

    upgraded app

--- app/src/main/java/com/rt/apps/kannada/MainActivity.java
+++ app/src/main/java/com/rt/apps/kannada/MainActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-        //////////////////////////////////
+
         // Find the View that shows the numbers category
         TextView numbers = (TextView) findViewById(R.id.numbers);
+
         // Set a click listener on that View
         numbers.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers category is clicked on.
@@ -23,13 +25,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Create a new intent to open the {@link NumbersActivity}
                 Intent numbersIntent = new Intent(MainActivity.this, Numbers.class);
+
                 // Start the new activity
                 startActivity(numbersIntent);
             }
         });
-///////////////////////////////////////
+
         // Find the View that shows the family category
         TextView family = (TextView) findViewById(R.id.family);
+
         // Set a click listener on that View
         family.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the family category is clicked on.
@@ -37,13 +41,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Create a new intent to open the {@link FamilyActivity}
                 Intent familyIntent = new Intent(MainActivity.this, Family_Members.class);
+
                 // Start the new activity
                 startActivity(familyIntent);
             }
         });
-//////////////////////////////////////////////////////
+
         // Find the View that shows the colors category
         TextView colors = (TextView) findViewById(R.id.colors);
+
         // Set a click listener on that View
         colors.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the colors category is clicked on.
@@ -51,13 +57,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Create a new intent to open the {@link ColorsActivity}
                 Intent colorsIntent = new Intent(MainActivity.this, Colors.class);
+
                 // Start the new activity
                 startActivity(colorsIntent);
             }
         });
-///////////////////////////////////////////////////////////////////////////////
+
         // Find the View that shows the phrases category
         TextView phrases = (TextView) findViewById(R.id.phrases);
+
         // Set a click listener on that View
         phrases.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the phrases category is clicked on.
@@ -65,11 +73,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Create a new intent to open the {@link PhrasesActivity}
                 Intent phrasesIntent = new Intent(MainActivity.this, Phrases.class);
+
                 // Start the new activity
                 startActivity(phrasesIntent);
             }
         });
-        ///////////////////////////////////////
     }
-
 }
---
+++ app/src/main/java/com/rt/apps/kannada/Word.java
@@ -0,0 +1,71 @@
+package com.rt.apps.kannada;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    /** Image resource ID for the word */
+    /** Image resource ID for the word */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /** Constant value that represents no image was provided for this word
+     * its value is -1 bcz -1 is out of the range of all possible cases*/
+    private static final int NO_IMAGE_PROVIDED = -1;
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+       /** * Create a new Word object.
+            *
+            * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+            * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image associated with the word
+     *
+             */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+}
